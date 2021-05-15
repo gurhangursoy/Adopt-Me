e4f59d454bf9c5b0fbbe20b9cb0a1a26c99a0f24
grhngrsoy <grhngrsoy@gmail.com>
2021-05-15 17:26:51 +0300

    Details page added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import SearchParams from './SearchParams';
 import Details from './Details';
@@ -8,8 +8,12 @@ import Details from './Details';
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me</h1>
       <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me</h1>
+          </Link>
+        </header>
         <Switch>
           <Route path="/details/:id">
             <Details />
--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,41 @@
-const Details = () => {
+import { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+class Details extends Component {
+  constructor(props) {
+    super();
+
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+    );
+  }
+  render() {
+      const {animal, breed, city, state, description, name } = this.state;
     return (
-        <h2>Hi!!!!</h2>
-    )
+        <div className="details">
+            <div>
+                <h1>{name}</h1>
+                <h2>{`${animal} - ${breed} - ${city} - ${state}`}</h2>
+                <button>Adopt {name}</button>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
+  }
 }
 
-export default Details
+export default withRouter(Details);
