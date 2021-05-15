699e1d431d66bae5293224cde285e8f51d23602b
grhngrsoy <grhngrsoy@gmail.com>
2021-05-15 16:40:25 +0300

    Router added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import ReactDOM from "react-dom";
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Pet from "./Pet"
-import SearchParams from "./SearchParams"
+import SearchParams from './SearchParams';
+import Details from './Details';
 
 const App = () => {
-    return (
-        <div>
-            <h1>Adopt Me</h1>
+  return (
+    <div>
+      <h1>Adopt Me</h1>
+      <Router>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route exact path="/">
             <SearchParams />
-        </div>
-    )
-}
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <StrictMode>
+    {' '}
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/Details.js
@@ -0,0 +1,7 @@
+const Details = () => {
+    return (
+        <h2>Hi!!!!</h2>
+    )
+}
+
+export default Details
